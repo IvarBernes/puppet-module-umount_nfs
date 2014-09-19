1cf909a5da4b9eade3db49856d7a4f77962c94cb
Ivar Bernes <eivarbe@sekalvd205.epk.ericsson.se>
2014-09-19 14:25:47 +0200

    Changed filename and variable names.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@
 # kills processes using a specified area and then unmounts that area.
 
 class puppet-module-umount_nfs (
-  $mountpoint = undef,
+  $arealist = undef,
 ){
-  $arealist = hiera_array("puppet-module-umount_nfs::mountpoint", undef)
-  puppet-module-umount_nfs::iterate {$arealist:}
+  $mountpoint = hiera_array("puppet-module-umount_nfs::arealist", undef)
+  puppet-module-umount_nfs::umount {$mountpoint:}
 }
---
+++ manifests/umount.pp
@@ -0,0 +1,20 @@
+# Defines a resource that kills processes and unmounts areas supplied via an array
+# exec fuser -> returns ["0","1"] due to that fuser exits with 1 if there are no processes to kill.
+
+define puppet-module-umount_nfs::umount (
+) {
+  if ! defined(Exec["fuser-$title"]) {
+    exec { "fuser-$title" :
+      command  => "fuser -k $title",
+      path     => ["/sbin", "/usr/sbin"],
+      returns  => ["0","1"],
+      before   => Mount["umount-$title"],
+    }
+  }
+  if ! defined(Mount["umount-$title"]) {
+    mount { "umount-$title" :
+      name    => $title,
+      ensure  => 'absent',
+    }
+  }
+}

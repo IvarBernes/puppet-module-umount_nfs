b7d79da0c68b79fc96dc508e1fb920db0d7a1cad
Ivar Bernes <eivarbe@sekalvd205.epk.ericsson.se>
2014-09-30 16:22:13 +0200

    Adding OS validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,51 @@ class umount_nfs (
   $kill_procs   = false,
   $hiera_merge  = true,
 ){
+  case $::osfamily {
+    'RedHat': {
+      case $::lsbmajdistrelease {
+        '5','6': {
+          #noop
+        }
+        default: {
+          fail("The umount_nfs module is supported by release 5, 6 and 7 of the RedHat Family. Your release is ${::lsbmajdistrelease}")
+        }
+      }
+    }
+    'Suse': {
+      case $::lsbmajdistrelease {
+        '10','11': {
+          #noop
+        }
+        default: {
+          fail("The umount_nfs module is supported by release 10 and 11 of the Suse Family. Your release is ${::lsbmajdistrelease}")
+        }
+      }
+    }
+    'Debian': {
+      case $::lsbmajdistrelease {
+        '12','13','14': {
+          #noop
+        }
+        default: {
+          fail("The umount_nfs module is supported by 12, 13 and 14 of the Debian Family. Your release is ${::lsbmajdistrelease}")
+        }
+      }
+    }
+    'Solaris': {
+      case $::kernelrelease {
+        '5.9','5.10': {
+          #noop
+        }
+        default: {
+          fail("The umount_nfs module supports Solaris kernel release 5.9, and 5.10. You are running ${::kernelrelease}.")
+        }
+      }
+    }
+    default: {
+      fail("The umount_nfs module is supported by the Debian, Redhat, Suse and Solaris Families. Your Family is ${::osfamily}")
+    }
+  }
   
   $hiera_merge_bool = type($hiera_merge) ? {
     'String' => str2bool($hiera_merge),

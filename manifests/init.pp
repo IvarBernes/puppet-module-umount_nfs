b6b48ffd6a6f334441772f5a42b1558b83c0ecad
Ivar Bernes <ivar.bernes@ericsson.com>
2014-11-11 13:31:07 +0100

    Removed OS check

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,52 +7,6 @@ class umount_nfs (
   $kill_procs   = false,
   $hiera_merge  = true,
 ){
-  case $::osfamily {
-    'RedHat': {
-      case $::lsbmajdistrelease {
-        '5','6': {
-          #noop
-        }
-        default: {
-          fail("The umount_nfs module is supported by release 5 and 6 of the RedHat Family. Your release is ${::lsbmajdistrelease}.")
-        }
-      }
-    }
-    'Suse': {
-      case $::lsbmajdistrelease {
-        '10','11': {
-          #noop
-        }
-        default: {
-          fail("The umount_nfs module is supported by release 10 and 11 of the Suse Family. Your release is ${::lsbmajdistrelease}.")
-        }
-      }
-    }
-    'Debian': {
-      case $::lsbdistrelease {
-        '12.04','14.04': {
-          #noop
-        }
-        default : {
-          fail("The umount_nfs module is supported by LTS release 12.04 and 14.04 of the Ubuntu Family. Your release is ${::lsbdistrelease}.")
-        }
-      }
-    }
-    'Solaris': {
-      case $::kernelrelease {
-        '5.10': {
-          #noop
-        }
-        default: {
-          fail("The umount_nfs module supports Solaris kernel release 5.10. You are running ${::kernelrelease}.")
-        }
-      }
-    }
-    default: {
-      fail("The umount_nfs module is supported by the Redhat, Suse, Solaris and Ubuntu Families. Your Family is ${::osfamily}.")
-    }
-  }
-
   $hiera_merge_bool = type($hiera_merge) ? {
     'String' => str2bool($hiera_merge),
     default  => $hiera_merge,
